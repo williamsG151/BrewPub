8f39d2c47fd3eaf50f86715e8d5b399fd92d9fff
Vianney Gall <vianneygall@gmail.com>
2021-03-27 17:29:16 -0400

    AnalSynt started

--- src/main/java/AnalLex.java
+++ src/main/java/AnalLex.java
@@ -68,7 +68,11 @@ public class AnalLex {
           state = 99;
           UL += current;
         }else if(P.contains(current)){
-          type = "P";
+          if("(".equals(current)){
+            type = "Pg";
+          }else{
+            type = "Pd";
+          }
           state = 99;
           UL += current;
         } else{
